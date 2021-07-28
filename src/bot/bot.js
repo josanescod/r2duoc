65d81e0cd60daf32ed30d9b7cf1cdcc070281c7d
josan <josanescod@gmail.com>
2021-07-28 19:39:06 +0200

    startWebhook

--- src/bot/bot.js
+++ src/bot/bot.js
@@ -1,7 +1,10 @@
 
 require('dotenv').config()
 const { Telegraf } = require("telegraf");
-const bot = new Telegraf(process.env.TOKEN);
+const PORT = process.env.PORT || 3000;
+const URL = process.env.URL || 'https://your-heroku-app.herokuapp.com';
+const API_TOKEN = process.env.API_TOKEN;
+const bot = new Telegraf(API_TOKEN);
 
 bot.launch()
 // Enable graceful stop
@@ -9,7 +12,9 @@ process.once('SIGINT', () => bot.stop('SIGINT'))
 process.once('SIGTERM', () => bot.stop('SIGTERM'))
 
 
-
+// Production
+//bot.telegram.setWebhook(`${URL}/bot${API_TOKEN}`);
+bot.startWebhook(`/bot${process.env.TOKEN}`, null, PORT)
 
 module.exports = bot;
 
